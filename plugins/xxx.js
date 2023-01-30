0f62c26975ad3c4f388396949008c35b38ec72ca
Diwaa01Boy <122137999+diwaa01boy@users.noreply.github.com>
2023-01-30 11:29:26 +0530

    Update xxx.js

--- plugins/xxx.js
+++ plugins/xxx.js
@@ -1,1 +1,16 @@
 /DIWAA BOY OFFICIAL WHATSAPP USERBOT/
+
+const { xxx,cmd } = require('../lib')
+cmd({
+        pattern: "xxx",
+        desc: "enjoy your life.",
+        category: "downloader",
+        filename: __filename
+    },
+    async(Void, citel,text,{isCreator}) => {
+if(!text) return citel.reply('Need post url.')
+let response = await xxx(text)
+for (let i=0;i<response.length;i++) {
+await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from xhamster.*`, citel)
+}
+    });
